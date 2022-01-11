15511a5c5de6aca4b40adbb712bd21bd843a6ef5
URBACZKA IVAN <46192105+IVAN-URBACZKA@users.noreply.github.com>
2022-01-11 11:00:15 +0100

    add tailwindcss

--- src/shopwinacs/settings.py
+++ src/shopwinacs/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 # Application definition
 
@@ -37,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'tailwind',
+    'theme'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +127,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
